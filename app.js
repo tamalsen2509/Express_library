15f07d147fb0594c23016de5c136edb8e1dc609a
tamal <tamal.sen@hotmail.com>
2020-10-10 00:51:40 +0530

    gitignore

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+require('dotenv').config()
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 let mongoose = require('mongoose');
-require('dotenv').config()
+
 
 
 
